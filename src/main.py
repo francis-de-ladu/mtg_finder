7025bf956b9c0a1f4400507a2996637f6c83bf91
Francis de Ladurantaye <fdl9044@gmail.com>
2023-02-18 21:51:41 -0500

    saving changes

--- src/main.py
+++ src/main.py
@@ -8,10 +8,6 @@ from tqdm import tqdm
 import re
 
 
-def is_public_prop(prop):
-    return not prop.startswith('_')
-
-
 def get_property(card, prop, key=None):
     try:
         get_prop_value = getattr(card, prop)
@@ -40,26 +36,24 @@ def to_card_properties(card):
     )
 
 
-def to_html(cards):
-    print(cards)
-
-
 def replace_symbols(text, svg_size=14):
     for match in re.findall(r"\{(\w+)\}", text):
+        filepath = f"assets/{match}.svg"
         try:
-            with open(f"assets/{match}.svg", 'r') as svg_file:
+            with open(filepath, 'r') as svg_file:
                 svg = svg_file.read()
 
             svg = re.sub(r'(width|height)="\d+"',
                          f"\\1={svg_size}", svg, count=2)
             text = text.replace(f"{{{match}}}", svg)
         except FileNotFoundError:
+            print(f"FileNotFoundError Exception --> {filepath}")
             continue
 
     return text
 
 
-def main():
+def main(top_n=5):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
@@ -68,7 +62,7 @@ def main():
     candidates = pd.read_csv("data/perrie/cards.csv")
 
     cards = []
-    for name in tqdm(candidates.card_name.tolist()[:10]):
+    for name in tqdm(candidates.card_name.tolist()[:top_n]):
         card = to_card_properties(scry.cards.Named(exact=name))
         cards.append(card)
         time.sleep(0.1)
@@ -81,24 +75,28 @@ def main():
         replace_symbols, svg_size=18)
     processed['text'] = processed['text'].apply(replace_symbols, svg_size=14)
 
+    processed['cmc'] = processed['cmc'].apply(
+        lambda x: int(x) or '').apply(str)
     processed['power'] = processed['power'].apply(lambda x: x or '')
     processed['toughness'] = processed['toughness'].apply(lambda x: x or '')
 
+    processed['url'] = processed['url'].apply(
+        lambda x: f'<a href="{x}">Gatherer</a>')
+
     styled = processed.drop(['id', 'lang'], axis=1).style.format(
-        formatter={'cmc': "{:.0f}"}
+        # formatter={'cmc': "{:.0f}"}
     )
 
     st.markdown(
         styled.to_html(),
         unsafe_allow_html=True
     )
-    markdown = to_html(processed)
 
-    with open("assets/2G.svg", 'r') as svg_file:
+    with open("assets/T.svg", 'r') as svg_file:
         svg = svg_file.read()
 
     st.title("supsupsup")
-    html = f"""<div style="height:20px"><p>supsup <span>{svg}</span> supsup</p></div>"""
+    html = f"<p>supsup <span>{svg}</span> supsup</p>"
     st.write(html, unsafe_allow_html=True)
 
     st.title("supsupsupsup")
